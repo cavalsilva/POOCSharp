727d40d145db8af52142e58a31e59399e73e24b4
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-12-15 06:31:31 -0200

    Atribuindo valores aos atributos da classe

--- BiteBank/01-BiteBank/Program.cs
+++ BiteBank/01-BiteBank/Program.cs
@@ -10,6 +10,18 @@ namespace _01_BiteBank
     {
         static void Main(string[] args)
         {
+            ContaCorrente contaDoRicardo = new ContaCorrente();
+
+            contaDoRicardo.titular = "Ricardo";
+            contaDoRicardo.agencia = 3032;
+            contaDoRicardo.numero = 346458;
+            contaDoRicardo.saldo = 100.00;
+
+            Console.WriteLine(contaDoRicardo.titular);
+            Console.WriteLine("Agência: "+ contaDoRicardo.agencia);
+            Console.WriteLine("Número: " + contaDoRicardo.numero);
+            Console.WriteLine("Saldo: " + contaDoRicardo.saldo);
+            Console.ReadLine();
         }
     }
 }
