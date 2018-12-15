72cfff696fc51d6291827e47b3785d396d0d9357
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-12-15 08:24:16 -0200

    Usando namespaces, criando classe Cliente e arrumando a classe ContaCorrente

---
+++ BiteBank/05-BiteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_BiteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente ricardo = new Cliente();
+            //ricardo.nome = "Ricardo";
+            //ricardo.profissao = "Desenvolvedor C#";
+            //ricardo.cpf = "012.345.678-90";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular.nome = "Ricardo";
+            conta.titular.profissao = "Desenvolvedor C#";
+            conta.titular.cpf = "012.345.678-90";
+            conta.agencia = 3032;
+            conta.numero = 346458;
+            conta.saldo = 1000;
+
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+
+             
+        }
+    }
+}
