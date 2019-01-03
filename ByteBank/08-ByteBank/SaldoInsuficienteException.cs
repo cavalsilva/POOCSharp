03d7a04b6c09a9fb4de63e250e5309b75394b6e0
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-01-02 23:01:16 -0200

    melhorando as exceptions

--- ByteBank/08-ByteBank/SaldoInsuficienteException.cs
+++ ByteBank/08-ByteBank/SaldoInsuficienteException.cs
@@ -8,10 +8,20 @@ namespace _08_ByteBank
 {
     public class SaldoInsuficienteException : Exception
     {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+
 
         public SaldoInsuficienteException()
         {
 
+        }
+
+        public SaldoInsuficienteException(double saldo, double valorSaque)
+            : this("Tentativa de saque no valor de " + valorSaque + " em uma conta com saldo de " + saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
         }
 
         public SaldoInsuficienteException(string mensagem) 
